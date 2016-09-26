098c8a9f395231fbd8c278ae89a6dc67ac035cdc
Christian Simon <simon@swine.de>
2016-09-26 16:50:55 +0100

    Refactor main code into package

--- kube2consul.go
+++ kube2consul.go
@@ -1,95 +1,16 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
-	kapi "k8s.io/kubernetes/pkg/api"
-	kcache "k8s.io/kubernetes/pkg/client/cache"
-	kclient "k8s.io/kubernetes/pkg/client/unversioned"
-	kframework "k8s.io/kubernetes/pkg/controller/framework"
-	kselector "k8s.io/kubernetes/pkg/fields"
-	"k8s.io/kubernetes/pkg/util/wait"
-
-	consulapi "github.com/hashicorp/consul/api"
+	"github.com/simonswine/kube2consul/pkg/kube2consul"
 )
 
-type kube2consul struct {
-	kubeClient    *kclient.Client
-	consulClient  *consulapi.Client
-	consulCatalog *consulapi.Catalog
-}
-
-func newKube2Consul(kc *kclient.Client, cc *consulapi.Client) *kube2consul {
-	k2c := &kube2consul{
-		kubeClient:    kc,
-		consulClient:  cc,
-		consulCatalog: cc.Catalog(),
+func main() {
+	k := kube2consul.New()
+	if err := k.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
 	}
-	return k2c
-}
-
-// watchForServices starts watching for new, removed or updated kubernetes services
-func (kc *kube2consul) watchForServices() kcache.Store {
-	serviceStore, serviceController := kframework.NewInformer(
-		kcache.NewListWatchFromClient(kc.kubeClient, "services", kapi.NamespaceAll, kselector.Everything()),
-		&kapi.Service{},
-		resyncPeriod,
-		kframework.ResourceEventHandlerFuncs{
-			AddFunc:    kc.newService,
-			DeleteFunc: kc.removeService,
-			UpdateFunc: kc.updateService,
-		},
-	)
-	go serviceController.Run(wait.NeverStop)
-	return serviceStore
-}
-
-// newService registers a new kubernetes service in Consul
-func (kc *kube2consul) newService(obj interface{}) {
-	if s, ok := obj.(*kapi.Service); ok {
-		log.Printf("Add Service %+v\n", s.GetName())
-		service := &consulapi.AgentService{
-			Service: s.GetName(),
-			Tags:    []string{"kubernetes"},
-		}
-		if len(s.Spec.Ports) > 0 {
-			service.Port = int(s.Spec.Ports[0].Port)
-		}
-		reg := &consulapi.CatalogRegistration{
-			Node:    s.Namespace,
-			Address: s.Spec.ClusterIP,
-			Service: service,
-			// Check: &consulapi.AgentCheck{
-			// 	ServiceName: s.GetName(),
-			// 	Name:        s.GetName() + " health check.",
-			// 	Status:      "unknown",
-			// },
-		}
-		wm, err := kc.consulCatalog.Register(reg, &consulapi.WriteOptions{})
-		if err != nil {
-			log.Println("Error registering service:", err)
-		} else {
-			log.Println(wm)
-		}
-	}
-}
-
-// removeService deregisters a kubernetes service in Consul
-func (kc *kube2consul) removeService(obj interface{}) {
-	if s, ok := obj.(*kapi.Service); ok {
-		log.Printf("Remove Service %+v\n", s.GetName())
-		service := &consulapi.CatalogDeregistration{
-			ServiceID: s.GetName(),
-			Node:      s.Namespace,
-		}
-		_, err := kc.consulCatalog.Deregister(service, &consulapi.WriteOptions{})
-		if err != nil {
-			log.Println("Error deregistering service:", err)
-		}
-	}
-}
-
-func (kc *kube2consul) updateService(oldObj, obj interface{}) {
-	kc.removeService(oldObj)
-	kc.newService(obj)
 }
---
+++ pkg/kube2consul/kube2consul.go
@@ -0,0 +1,197 @@
+package kube2consul
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/cobra"
+	kapi "k8s.io/kubernetes/pkg/api"
+	kcache "k8s.io/kubernetes/pkg/client/cache"
+	kubernetes "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_3"
+	krest "k8s.io/kubernetes/pkg/client/restclient"
+	kclient "k8s.io/kubernetes/pkg/client/unversioned"
+	kclientcmd "k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
+	kframework "k8s.io/kubernetes/pkg/controller/framework"
+	kselector "k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/util/wait"
+
+	"github.com/simonswine/kube2consul/pkg/detect_node"
+	"github.com/simonswine/kube2consul/pkg/interfaces"
+	//"github.com/simonswine/kube2consul/pkg/services"
+)
+
+var AppVersion string = "unknown"
+var AppName string = "kube2consul"
+
+type Kube2Consul struct {
+	RootCmd             *cobra.Command
+	kubernetesClient    *kclient.Client
+	kubernetesClientset *kubernetes.Clientset
+	kubernetesConfig    *krest.Config
+	Kubeconfig          string
+	detectNode          *detect_node.DetectNode
+	resyncPeriod        time.Duration
+}
+
+var _ interfaces.Kube2Consul = &Kube2Consul{}
+
+func New() *Kube2Consul {
+	k := &Kube2Consul{
+		resyncPeriod: 30 * time.Second,
+	}
+	k.init()
+	return k
+}
+
+func (k *Kube2Consul) userHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+func (k *Kube2Consul) NodeIPByPodIP(podIP string) (nodeIP string, err error) {
+	return k.detectNode.NodeIPByPodIP(podIP)
+}
+
+func (k *Kube2Consul) init() {
+
+	log.SetOutput(os.Stderr)
+	log.SetLevel(log.DebugLevel)
+
+	k.RootCmd = &cobra.Command{
+		Use:   "kube2consul",
+		Short: "Export Kubernetes NodePort services to Consul",
+		Run: func(cmd *cobra.Command, args []string) {
+			k.cmdRun()
+		},
+	}
+	k.RootCmd.PersistentFlags().StringVarP(
+		&k.Kubeconfig,
+		"kubeconfig",
+		"k",
+		filepath.Join(k.userHomeDir(), ".kube/config"),
+		"path to the kubeconfig file",
+	)
+
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: fmt.Sprintf("Print the version number of %s", AppVersion),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("%s version %s\n", AppName, AppVersion)
+		},
+	}
+
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List servicecs that whould have been registered in consul",
+		Run: func(cmd *cobra.Command, args []string) {
+			k.cmdList()
+		},
+	}
+
+	k.RootCmd.AddCommand(versionCmd)
+	k.RootCmd.AddCommand(listCmd)
+
+	k.detectNode = detect_node.New(k)
+
+}
+
+func (k *Kube2Consul) cmdList() {
+	svcs, err := k.KubernetesClientset().Core().Services("").List(kapi.ListOptions{})
+	if err != nil {
+		log.Printf("err: %s", err)
+	}
+
+	for _, svc := range svcs.Items {
+		if kapi.ServiceType(svc.Spec.Type) == kapi.ServiceTypeNodePort {
+			fmt.Printf("%s/%s\n", svc.Namespace, svc.Name)
+		}
+	}
+}
+
+func (k *Kube2Consul) cmdRun() {
+	k.watchForServices()
+	select {}
+}
+
+func (k *Kube2Consul) KubernetesConfig() *krest.Config {
+	if k.kubernetesConfig == nil {
+		// try in cluster first
+		config, err := krest.InClusterConfig()
+		if err != nil {
+			log.Warnf("Failed to connect to kubernetes using in-cluster configuration: %s", err)
+			log.Infof("Trying configuration from %s", k.Kubeconfig)
+			// uses the current context in kubeconfig
+			config, err = kclientcmd.BuildConfigFromFlags("", k.Kubeconfig)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+		k.kubernetesConfig = config
+	}
+	return k.kubernetesConfig
+}
+
+func (k *Kube2Consul) KubernetesClient() *kclient.Client {
+	if k.kubernetesClient == nil {
+		client, err := kclient.New(k.KubernetesConfig())
+		if err != nil {
+			panic(err.Error())
+		}
+		k.kubernetesClient = client
+	}
+	return k.kubernetesClient
+}
+
+func (k *Kube2Consul) KubernetesClientset() *kubernetes.Clientset {
+	if k.kubernetesClientset == nil {
+		clientset, err := kubernetes.NewForConfig(k.KubernetesConfig())
+		if err != nil {
+			panic(err.Error())
+		}
+		k.kubernetesClientset = clientset
+	}
+	return k.kubernetesClientset
+}
+
+func (k *Kube2Consul) watchForServices() kcache.Store {
+	serviceStore, serviceController := kframework.NewInformer(
+		kcache.NewListWatchFromClient(k.KubernetesClient(), "services", kapi.NamespaceAll, kselector.Everything()),
+		&kapi.Service{},
+		k.resyncPeriod,
+		kframework.ResourceEventHandlerFuncs{
+			AddFunc:    k.newService,
+			DeleteFunc: k.removeService,
+			UpdateFunc: k.updateService,
+		},
+	)
+	go serviceController.Run(wait.NeverStop)
+	return serviceStore
+}
+
+func (k *Kube2Consul) newService(obj interface{}) {
+	if s, ok := obj.(*kapi.Service); ok {
+		log.Printf("Add Service %+v\n", s.GetName())
+	}
+}
+
+// removeService deregisters a kubernetes service in Consul
+func (k *Kube2Consul) removeService(obj interface{}) {
+	if s, ok := obj.(*kapi.Service); ok {
+		log.Printf("Remove Service %+v\n", s.GetName())
+	}
+}
+
+func (k *Kube2Consul) updateService(oldObj, obj interface{}) {
+	k.removeService(oldObj)
+	k.newService(obj)
+}
